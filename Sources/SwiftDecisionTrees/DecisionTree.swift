d47b31fe5bd4e75cae9ceb3877d07670ec5acf12
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2021-06-24 13:16:41 +1000

    hacky fix for obsure c45 wieghts bug

--- Sources/SwiftDecisionTrees/DecisionTree.swift
+++ Sources/SwiftDecisionTrees/DecisionTree.swift
@@ -1058,8 +1058,12 @@ public func insideRules(data : DataSet, rules : PathToNode) -> DataSet {
                 withMissingValue.append(i)
             }
         }
+        var newWeight = numIncluded/(numIncluded+numExcluded)
+        if(newWeight.isNaN) {
+            newWeight = 1.0
+        }
         for i in withMissingValue {
-            weights[i] =  (weights[i] ?? 1.0) * (numIncluded/(numIncluded+numExcluded))
+            weights[i] =  (weights[i] ?? 1.0) * newWeight
         }
     }
     for i in 0..<data.instances.count {
